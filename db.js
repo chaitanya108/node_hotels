56b556536942a5a4d27925ad1970a511007a667f
Chaitanya Pawase <chaitanyapawase08@gmail.com>
2024-05-16 16:15:10 +0530

    third version

--- db.js
+++ db.js
@@ -2,14 +2,15 @@
 const mongoose = require('mongoose');
 
 //defining mongodb connection url
-const mongoURL = 'mongodb://0.0.0.0:27017/hotels';
-
+//const mongoURL = process.env.MONGODB_URL_LOCA;
+const mongoURL = process.env.MONGODB_URL;
 //setup mongodb connection
 mongoose.connect(mongoURL);
 
 
 //get default connection
 const db = mongoose.connection;
+
 
 //event listeners
 db.on('error', (error) => {
--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const app = express()
-const db = require('./db')
-
-const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const db = require("./db");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
-const person = require('./models/person')
-
-const personRoutes = require('./routes/personRoutes')
-
-app.get('/', function(req, res){
-    res.send("Welcome honey")
+const person = require("./models/person");
+const personRoutes = require("./routes/personRoutes");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+app.get("/", function (req, res) {
+  res.send("Welcome honey");
 });
 
-app.use('/person', personRoutes);
+app.use("/person", personRoutes);
 
-//commen
-app.listen(3000, ()=>{
-    console.log('listening on port 3000')
+//comment
+app.listen(3000, () => {
+  console.log("listening on port 3000");
 });
